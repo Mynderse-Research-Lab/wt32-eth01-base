12d2bb34902da5cafb86c897e4336a56b844b115
Travis-Bowman <tbowman@ltu.edu>
2025-06-30 19:25:57 -0400

    Created a config for Network params

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <ETH.h>
+#include <PubSubClient.h>
+#include "config.hpp"
 
-#define ETH_ADDR        1
-#define ETH_POWER_PIN   16   // Set only once
-#define ETH_MDC_PIN     23
-#define ETH_MDIO_PIN    18
-#define ETH_TYPE        ETH_PHY_LAN8720
-#define ETH_CLK_MODE    ETH_CLOCK_GPIO17_OUT
+// MQTT client
+WiFiClient ethClient;
+PubSubClient client(ethClient);
 
+// Event: Print IP once obtained
 void WiFiEvent(WiFiEvent_t event) {
   if (event == ARDUINO_EVENT_ETH_GOT_IP) {
     Serial.print("IP: ");
@@ -14,36 +14,69 @@ void WiFiEvent(WiFiEvent_t event) {
   }
 }
 
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("WT32Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" retrying in 2s");
+      delay(2000);
+    }
+  }
+}
+
 void setup() {
+
+  // Setting up serial comms
   Serial.begin(115200);
   delay(1000);
   Serial.println("Starting ETH...");
 
+  // Setting power pins to turn on the LAN8720A chip
   pinMode(ETH_POWER_PIN, OUTPUT);
   digitalWrite(ETH_POWER_PIN, HIGH);
   delay(100);
 
-  WiFi.onEvent(WiFiEvent);  // Register event callback
 
-  // Initialize Ethernet driver first
-  ETH.begin(
-    ETH_ADDR,
-    ETH_POWER_PIN,
-    ETH_MDC_PIN,
-    ETH_MDIO_PIN,
-    ETH_TYPE,
-    ETH_CLK_MODE
+  WiFi.onEvent(WiFiEvent);
+
+  // Setting up ethernet comms
+  ETH.begin(ETH_ADDR, 
+            ETH_POWER_PIN,
+            ETH_MDC_PIN, 
+            ETH_MDIO_PIN, 
+            ETH_TYPE, 
+            ETH_CLK_MODE
   );
-
-  // Now assign static IP configuration
+  
   IPAddress local_ip(192, 168, 2, 2);
-  IPAddress gateway(192, 168, 2, 1);  // Your laptop
+  IPAddress gateway(192, 168, 2, 1); 
   IPAddress subnet(255, 255, 255, 0);
   ETH.config(local_ip, gateway, subnet);
-}
 
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+}
 
 void loop() {
-  delay(1000);
+  
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  client.loop();
+
+  // Send a message every 5 seconds
+  static unsigned long lastMsg = 0;
+  if (millis() - lastMsg > 5000) {
+    lastMsg = millis();
+    String msg = "hello from WT32";
+    Serial.print("Publishing: ");
+    Serial.println(msg);
+    client.publish(MQTT_TOPIC_TEST, msg.c_str());
+  }
 }
+
 
